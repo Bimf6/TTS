fd372c874b7f54aa255d9c4bb150449b0d7ccea3
Michelin Lam <michelinlam@proton.me>
2025-10-07 19:26:26 +0800

    Declare both 'app' and 'streamlit_app' packages for installer compatibility; clean up

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="app",
-    version="0.0.0",
-    packages=find_packages(),
-    install_requires=[],
-    python_requires=">=3.8",
-)
